948ab140aa35abd8ee712daae5f6b0367f907038
Ben Beecher <benbeecher@gmail.com>
2012-08-23 09:35:36 -0400

    getting server to work locally

--- settings.py
+++ settings.py
@@ -167,28 +167,7 @@ STATICFILES_FINDERS = (
 #############
 # DATABASES #
 #############
-import json
-with open('/home/dotcloud/environment.json') as f:
-  env = json.load(f)
-
-
-if env["DOTCLOUD_PROJECT"] != "zombies":
-    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-    DEBUG = True
-else:
-    EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tutelage',
-        'USER': env['DOTCLOUD_DATA_SQL_LOGIN'],
-        'PASSWORD': env['DOTCLOUD_DATA_SQL_PASSWORD'],
-        'HOST': env['DOTCLOUD_DATA_SQL_HOST'],
-        'PORT': int(env['DOTCLOUD_DATA_SQL_PORT']),
-    }
-}
+#handled by local settings or prod settings
 
 #########
 # PATHS #
@@ -358,7 +337,8 @@ DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 try:
     from local_settings import *
 except ImportError:
-    pass
+    from prod_settings import *
+    
 
 
 ####################
