ddf0ab6b297dc3d3a74f2e43f9479232cf9536b4
Ben Beecher <benbeecher@gmail.com>
2013-02-24 22:56:34 -0500

    shoot, left a pdb inush

--- app/internal_stripe.py
+++ app/internal_stripe.py
@@ -4,8 +4,6 @@ from django.conf import settings
 stripe.api_key = settings.STRIPE_API
 
 def create_customer(user, token_id):
-    import pdb
-    pdb.set_trace()
     customer = stripe.Customer.create(
         card=token_id,
         description=user.email,
