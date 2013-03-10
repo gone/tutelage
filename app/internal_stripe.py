9659cbc0a417c27e1d080260cace22754b53b812
Ben Beecher <benbeecher@gmail.com>
2013-03-09 20:35:07 -0500

    purchases must be greater than 50c

--- app/internal_stripe.py
+++ app/internal_stripe.py
@@ -8,17 +8,19 @@ def create_customer(user, token_id):
         card=token_id,
         description=user.email,
     )
+
     return customer
 
 def purchase_lesson(user, lesson):
     customer_id = user.customer.customer_id
     amount = int(lesson.price) * 100 #convert to cents
     #TODO: save the receipt
-    stripe.Charge.create(
-        amount=amount,
-        currency="usd",
-        customer=customer_id,
-    )
+    if amount > 50:
+        stripe.Charge.create(
+            amount=amount,
+            currency="usd",
+            customer=customer_id,
+            )
 
 
 def bill_pledge(pledge):
